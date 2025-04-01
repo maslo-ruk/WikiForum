1e182c27a23276204b8b50f6cd3371a883ef1c65
Egot_Zhavoronkov <georgelarkenston@gmail.com>
2025-04-01 12:37:39 +0300

    добавлена навигация по тегам

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ app.config['UPLOAD_FOLDER'] = 'materials'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     number = 1
