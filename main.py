d6b2d5b09f5c7bd24af942148ca7d0c0f6214063
Egot_Zhavoronkov <georgelarkenston@gmail.com>
2025-04-16 21:43:06 +0300

    добавил лайки

--- main.py
+++ main.py
@@ -129,23 +129,24 @@ def logout():
 
 @app.route('/add_post', methods=['GET', 'POST'])
 def add_postt():
-    name = request.form['title']
-    story = request.form["story"]
-    files = request.form["files"]
-    idd = current_user.id
-    add_post(name, story, [1], 1)
-    for i, file in enumerate(files):
-        if os.path.isfile(file) and file.lower().endswith('.png'): #проверка файла
-            filename = os.path.basename(file)
-            name, tg = os.path.splitext(filename)
-            if len(files) > 1:
-                filename_pic = f"{idd}_{name}_({index + 1}).png"
+    if request.method == 'POST':
+        name = request.form['title']
+        story = request.form["story"]
+        files = request.form["files"]
+        idd = current_user.id
+        add_post(name, story, [1], 1)
+        for i, file in enumerate(files):
+            if os.path.isfile(file) and file.lower().endswith('.png'): #проверка файла
+                filename = os.path.basename(file)
+                name, tg = os.path.splitext(filename)
+                if len(files) > 1:
+                    filename_pic = f"{idd}_{name}_({index + 1}).png"
+                else:
+                    filename_pic = f"{idd}_{name}.png"
+                shutil.copy(file, os.path.join('images', filename_pic))
             else:
-                filename_pic = f"{idd}_{name}.png"
-            shutil.copy(file, os.path.join('images', filename_pic))
-        else:
-            print(f"Такого файла нет")
-    return render_template('add_post-2.html', post=post)
+                print(f"Такого файла нет")
+    return render_template('add_post-2.html')
 
 @app.route('/post/<id>')
 def post(id):
